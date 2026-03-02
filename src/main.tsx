22d97261a9c694717c277edbfc4082208f0bc19d
darriwil <ysk.u.xcviii@gmail.com>
2026-03-02 09:27:43 +0900

    add: PrimeReact導入

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,24 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { PrimeReactProvider } from 'primereact/api'
+import 'primereact/resources/themes/soho-dark/theme.css'
+import 'primereact/resources/primereact.min.css'
+import 'primeicons/primeicons.css'
 import './index.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <PrimeReactProvider
+      value={{
+        pt: {
+          button: {
+            root: { className: 'p-button-sm' },
+          },
+        },
+      }}
+    >
+      <App />
+    </PrimeReactProvider>
   </StrictMode>
 )
