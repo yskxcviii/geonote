2f25de8549ad06e73fe5becc04749da9adea1c9d
wvkizvshi <ysk.u.xcviii@gmail.com>
2026-03-08 18:03:50 +0900

    update: ロゴ追加 + トップ画面修正

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import Logo from './assets/geonote-logo.png'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <div>
         <a href="https://vite.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+          <img src={Logo} className="logo" alt="logo" />
         </a>
       </div>
       <h1>GeoNote</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
+      <div>🚧 Under construction...  🚧</div>
     </>
   )
 }
