a1f0b7d95f6753aacce96d3284b00930d77b2f7e
wvkizvshi <ysk.u.xcviii@gmail.com>
2026-03-13 19:12:20 +0900

    update: App.tsxを修正

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,26 @@
 import Logo from './assets/geonote-logo.png'
 
-function App() {
+const App = () => {
   return (
-    <>
+    <div
+      style={{
+        width: '100vw',
+        height: '100vh',
+        backgroundColor: 'var(--surface-ground)',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}
+    >
       <div>
         <a href="https://vite.dev" target="_blank" rel="noreferrer">
-          <img src={Logo} className="logo" alt="logo" />
+          <img src={Logo} alt="logo" style={{ width: '100px', height: '100px' }} />
         </a>
       </div>
       <h1>GeoNote</h1>
       <div>🚧 Under construction... 🚧</div>
-    </>
+    </div>
   )
 }
 
